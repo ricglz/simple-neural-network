9a792db1f2b0a0c96fcc3a339afb2074918e6ceb
Ricardo J González C <rjgcastillo@hotmail.com>
2020-11-26 19:37:03 -0600

    Add new code for deciding best per layer configuration

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 """
 Main module to train the network for the game
 """
-from numpy import array, load
+from numpy import argmin, array, load
 from pandas import read_csv
 import matplotlib.pyplot as plt
 
@@ -25,18 +25,22 @@ def training(net, training_dataset):
 
     rmses, val_rmses = [], []
     try:
-        while True:
-            epochs = 10
-            print(f'Learning rate: {net.learning_rate}')
+        cur_rmses, cur_val_rmses = net.fit(training_dataset, validation_dataset, 50)
+        rmses += cur_rmses[1:]
+        val_rmses += cur_val_rmses[1:]
 
-            cur_rmses, cur_val_rmses = net.fit(training_dataset, validation_dataset, epochs)
-            rmses += cur_rmses[1:]
-            val_rmses += cur_val_rmses[1:]
+        # while True:
+        #     epochs = 10
+        #     print(f'Learning rate: {net.learning_rate}')
 
-            if len(cur_rmses) < epochs + 1:
-                net.learning_rate /= 2
-            else:
-                net.learning_rate *= 1.1
+        #     cur_rmses, cur_val_rmses = net.fit(training_dataset, validation_dataset, epochs)
+        #     rmses += cur_rmses[1:]
+        #     val_rmses += cur_val_rmses[1:]
+
+        #     if len(cur_rmses) < epochs + 1:
+        #         net.learning_rate /= 2
+        #     else:
+        #         net.learning_rate *= 1.1
 
     except KeyboardInterrupt:
         print()
@@ -44,6 +48,44 @@ def training(net, training_dataset):
     print(f'Final RMSE: {net.calculate_rmse(training_dataset):.4%}')
 
     return rmses, val_rmses
+
+def create_network_from_layers(layers):
+    assert len(layers) >= 1, 'Layers array is empty :('
+
+    network = NeuralNetwork()
+    network.add_layer(layers[0], 2)
+    for index in range(1, len(layers)):
+        network.add_layer(layers[index])
+    network.add_layer(2)
+    return network
+
+def create_network_permutations(layer_count, networks=None, current_layers=None):
+    if current_layers is None:
+        current_layers = []
+        networks = []
+    for value in range(1, 11):
+        new_layers = current_layers + [value]
+        if layer_count == 1:
+            print('Creating new network: ', new_layers)
+            network = create_network_from_layers(new_layers)
+            networks.append(network)
+        else:
+            create_network_permutations(layer_count - 1, networks, new_layers)
+    return networks
+
+def train_layer_networks(networks, training_dataset):
+    def train(network):
+        print('Training:\n', network)
+        rmses, val_rmses = training(network, training_dataset)
+        return rmses[-1] + val_rmses[-1] / 2
+    return array(list(map(train, networks)))
+
+def decide_layer_config(layer_count, training_dataset):
+    networks = create_network_permutations(layer_count)
+    final_rmses = train_layer_networks(networks, training_dataset)
+    best_network_index = argmin(final_rmses)
+    best_network = networks[best_network_index]
+    return best_network
 
 def plot(rmses, val_rmses, h_layers):
     """
@@ -63,9 +105,10 @@ def plot(rmses, val_rmses, h_layers):
 
     plt.show()
 
-def create_network(filename):
+def create_network_from_filename(filename):
     """
     Create network based on the weights stored in filename
+
     @type filename: str
     """
     weights = load(filename, allow_pickle=True)
@@ -80,7 +123,7 @@ def save_best_layer(net, training_dataset):
     architecture_filename = get_arch_numpy(len(net.layers) - 1)
     print(f'Saving {architecture_filename}')
     try:
-        current_best_network = create_network(architecture_filename)
+        current_best_network = create_network_from_filename(architecture_filename)
         net_rmse = net.calculate_rmse(training_dataset)
         best_rmse = current_best_network.calculate_rmse(training_dataset)
         if net_rmse < best_rmse:
@@ -93,6 +136,11 @@ def save_best_layer(net, training_dataset):
         net.save(architecture_filename)
 
 def decide_best_from_array(networks, training_dataset):
+    """
+    Based on a list of networks decide which is the best,
+    being classified as the best the one with the lowest
+    rmse
+    """
     best_network = networks[0]
     best_rmse = best_network.calculate_rmse(training_dataset)
     print(f'{best_network}\nRMSE {best_rmse:.4%}\n')
@@ -103,7 +151,7 @@ def decide_best_from_array(networks, training_dataset):
         if best_rmse > current_rmse:
             best_network = current_network
             best_rmse = current_rmse
-    return best_network
+    return best_network, best_rmse
 
 def decide_best_architecture(training_dataset):
     """
@@ -113,22 +161,25 @@ def decide_best_architecture(training_dataset):
     print('Deciding best architecture\n')
     max_current_layer = 4
     filenames = list(map(get_arch_numpy, list(range(1, max_current_layer + 1))))
-    networks = list(map(create_network, filenames))
-    best_network = decide_best_from_array(networks, training_dataset)
+    networks = list(map(create_network_from_filename, filenames))
+    best_network, best_rmse = decide_best_from_array(networks, training_dataset)
 
     print(f'{best_network}\nRMSE {best_rmse:.4%}\n')
     best_network.save('model_weights.npy')
 
 def main():
     """Main procedure for building NN"""
-    # net = create_network(get_arch_numpy(4))
+    # net = create_network_from_filename(get_arch_numpy(4))
+
     training_dataset = split_dataset(array(read_csv('./training_data.csv', header=None)))
 
-    for index in range(1, 5):
-        net = create_network(get_arch_numpy(index))
-        print(net)
-        training(net, training_dataset)
-        save_best_layer(net, training_dataset)
+    decide_layer_config(1, training_dataset)
+
+    # for index in range(1, 5):
+    #     net = create_network_from_filename(get_arch_numpy(index))
+    #     print(net)
+    #     training(net, training_dataset)
+    #     save_best_layer(net, training_dataset)
 
     # print('Current architecture\n', net)
 
@@ -137,7 +188,7 @@ def main():
 
     # save_best_layer(net, training_dataset)
 
-    decide_best_architecture(training_dataset)
+    # decide_best_architecture(training_dataset)
 
 if __name__ == "__main__":
     main()
